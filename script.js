6377915a3a9a05cd6a79222fcab8d7119d5bb4b9
DamDamWCS <bobon.damien.wcs@gmail.com>
2022-10-07 14:56:40 +0200

    ajout de la fonction sticky au header + modif script

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function toggleMenu () {  
     const navbar = document.querySelector('.navbar');
     const burger = document.querySelector('.burger');
+    const header = document.querySelector('header');
     
     burger.addEventListener('click', (e) => {    
       navbar.classList.toggle('show-nav');
+      header.classList.toggle('show-nav');
     });    
     // const navbarLinks = document.querySelectorAll('.navbar a');
     // navbarLinks.forEach(link => {
