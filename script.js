b7cb1c8d87a2da65f4c3281bc9c9ddb380d93ead
DamDamWCS <bobon.damien.wcs@gmail.com>
2022-10-20 14:55:35 +0200

    ajout d'un fichier .gitignire

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
+// BURGER MENU
 function toggleMenu () {  
     const navbar = document.querySelector('.navbar');
     const burger = document.querySelector('.burger');
     const header = document.querySelector('header');
     
-    // burger.addEventListener('click', (e) => {    
-    //   navbar.classList.toggle('show-nav');
-    //   header.classList.toggle('show-nav');
-    // });    
     burger.addEventListener('click',function() {    
       navbar.classList.toggle('show-nav');
       header.classList.toggle('show-nav');
@@ -15,3 +12,13 @@ function toggleMenu () {
   }
   toggleMenu();
 
+  // FORMULAIRE DE CONTACTE
+  const formOk =
+  "Merci de votre message, il sera traité prochainement.";
+
+document.querySelector(".contactForm");
+document.addEventListener("submit", function (event) {
+  event.preventDefault();
+  document.querySelector(".contactForm").reset();
+  alert(formOk);
+});
