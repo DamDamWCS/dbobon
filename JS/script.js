4b91c7a45e99a97c9066d855d35327bc642af80f
DamDamWCS <bobon.damien.wcs@gmail.com>
2022-10-23 22:35:55 +0200

    modification du JS pour remplacer la regex et modification de l'arborescence des fichiers

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,8 @@ function toggleMenu() {
   const header = document.querySelector("header");
 
   burger.addEventListener("click", function () {
-    navbar.classList.toggle("show-nav");
-    header.classList.toggle("show-nav");
+    navbar.classList.toggle("showNav");
+    header.classList.toggle("showNav");
   });
 }
 toggleMenu();
@@ -31,16 +31,19 @@ document.addEventListener("submit", function (event) {
 //   });
 // });
 
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+//NAVIGATION DES LIENS D'ANCRES 
+const anchorLinks = document.querySelectorAll(".anchorLink");
+anchorLinks.forEach((anchorLink) => {
+  anchorLink.addEventListener("click", function (e) {
     const hash = e.currentTarget.hash; // Récupère la valeur du href du lien
     const nameHash = document.querySelector(hash); // selection de l'élement du lien de l'ancre
-      e.preventDefault();
-      console.log(hash);
-      console.log(nameHash.offsetTop); // J'affiche la position top de l'élement cible de mon lien
-      window.scrollTo({
-        top: nameHash.offsetTop - 85, //Je défini la postion top à la quel me rendre - la hauteur de mon header (85px)
-        behavior: "smooth", // J'ajoute un effet doux à la navigation
-      });
+    e.preventDefault();
+    console.log(anchorLinks);
+    console.log(hash);
+    console.log(nameHash.offsetTop); // J'affiche la position top de l'élement cible de mon lien
+    window.scrollTo({ //Positionnement de la fenêtre
+      top: nameHash.offsetTop - 85, //Je défini la postion top à la quel me rendre - la hauteur de mon header (85px)
+      behavior: "smooth", // J'ajoute un effet doux à la navigation
+    });
   });
 });
