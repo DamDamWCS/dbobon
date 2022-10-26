619ec3c8b2191b98f198641cc94222eb0febfe49
DamDamWCS <bobon.damien.wcs@gmail.com>
2022-10-26 10:54:50 +0200

    modification des liens en absolu pour les fonts

--- JS/script.js
+++ JS/script.js
@@ -48,3 +48,25 @@ document.querySelectorAll(".anchorLink").forEach(anchorLink => {
     });
   });
 });
+
+
+//ANIMATION DES SECTION
+
+// First we select the element we want to target
+const target = document.querySelector(".animated");
+function handleIntersection(entries) {
+  // The callback will return an array of entries, even if you are only observing a single item
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible')
+    } else {
+      entry.target.classList.remove('visible')
+    }
+  });
+}
+// Next we instantiate the observer with the function we created above. This takes an optional configuration
+// object that we will use in the other examples.
+const observer = new IntersectionObserver(handleIntersection);
+
+// Finally start observing the target element
+observer.observe(target);
