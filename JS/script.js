181e0125dc5a2e1aa4cd7f555b78ce010684209c
DamDamWCS <damien.bobon.wcs@gmail.com>
2023-02-02 22:08:27 +0100

    finalisation

--- JS/script.js
+++ JS/script.js
@@ -12,15 +12,14 @@ toggleMenu();
 //NAVIGATION DES LIENS D'ANCRES
 document.querySelectorAll(".anchorLink").forEach((anchorLink) => {
   anchorLink.addEventListener("click", function (e) {
-    const hash = e.currentTarget.hash; 
-    const nameHash = document.querySelector(hash); 
+    const hash = e.currentTarget.hash;
+    const nameHash = document.querySelector(hash);
     e.preventDefault();
     console.log(hash);
-    console.log(nameHash.offsetTop); 
+    console.log(nameHash.offsetTop);
     window.scrollTo({
-      top: nameHash.offsetTop - 85, 
-      behavior: "smooth", 
+      top: nameHash.offsetTop - 85,
+      behavior: "smooth",
     });
   });
 });
-
